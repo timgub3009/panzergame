b48e4571de8594d0e44b41a9c7cea571b666af6e
Gubeidulin Timur <timur3009@yandex.ru>
2023-01-14 18:02:28 +0300

    raw version

---
+++ script.js
@@ -0,0 +1,117 @@
+// set of variables created for the game grabbed from the DOM
+
+let firstPlayerName = document.querySelector('.elements__heading_type_first-player');
+let secondPlayerName = document.querySelector('.elements__heading_type_second-player');
+let firstPlayerHealthBar = document.querySelector('.elements__health-bar_type_first-player');
+let secondPlayerHealthBar = document.querySelector('.elements__health-bar_type_second-player');
+let result = document.querySelector('.results__text');
+let resetBtn = document.querySelector('.results__reset');
+
+
+// function for updating the DOM with the names and current states of health of the players
+const updateGame = (player1, player2, gameState) => {
+    //grab the names, grab the health
+    firstPlayerName.textContent = player1.name;
+    secondPlayerName.textContent = player2.name;
+    firstPlayerHealthBar.textContent = player1.health;
+    secondPlayerHealthBar.textContent = player2.health;
+    //if health is 0 the game is over
+    if (player1.health <= 0 || player2.health <= 0) {
+        game.isOver = true;
+        gameState = game.isOver;
+        result.innerText = game.declareWinner(game.isOver, player1, player2);
+        return gameState;
+    }
+}
+
+class Player {
+    constructor(name, health, attackingDamage) {
+        this.name = name;
+        this.health = health;
+        this.attackingDamage = attackingDamage;
+    }
+
+    shoot (player, enemy, attackingDamage) {
+        //random amount of damage 
+        let damageAmount = Math.floor(Math.random() * 10);
+        //count the damage that we need to substract from the health
+        enemy.health -= damageAmount;
+        //update the game after this move
+        updateGame(player1, player2, game.isOver);
+        //return the message with damage
+        return `${player.name} attacks ${enemy.name} for ${damageAmount} damage`;
+    }
+
+    repair (player) {
+        let healAmount = Math.floor(Math.random() * 5);
+        //the reaction of the health bar 
+        player.health += healAmount;
+        //update the game
+        updateGame(player1, player2, game.isOver);
+        //return the message with hp
+        return `${player.name} repaired for ${healAmount} hitpoints`
+    }
+
+}
+
+let player1 = new Player('Tiger', 100, 10);
+let player2 = new Player('T34', 100, 10);
+
+
+
+class Game {
+    constructor() {
+        this.isOver = false;
+    }
+    declareWinner(isOver, player1, player2) {
+        let message;
+        if (isOver === true && player1.health <= 0) {
+            message = `${player2.name} WINS!`
+        }
+        else if (isOver === true && player2.health <= 0) {
+            message = `${player1.name} WINS!`
+        }
+        return message;
+    }
+
+    reset(player1, player2) {
+        player1.health = 100;
+        player2.health = 100;
+        this.isOver = false;
+        result.textContent = "";
+        updateGame(player1, player2);
+    }
+}
+
+//create game
+let game = new Game();
+let gameState = game.isOver;
+
+//initialize the game by calling updateGame
+updateGame(player1, player2, game.isOver);
+
+document.addEventListener('keydown', (evt) => {
+    if(evt.key === 'q' && player2.health > 0 && game.isOver === false) {
+        player1.shoot(player1, player2, player1.attackingDamage);
+    }
+})
+
+document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'a' && player2.health > 0 && game.isOver === false) {
+        player1.repair(player1);
+    }
+})
+
+document.addEventListener('keydown', (evt) => {
+    if(evt.key === 'w' && player1.health > 0 && game.isOver === false) {
+        player2.shoot(player2, player1, player2.attackingDamage);
+    }
+})
+
+document.addEventListener('keydown', (evt) => {
+    if (evt.key === 's' && player1.health > 0 && game.isOver === false) {
+        player2.repair(player2);
+    }
+})
+
+resetBtn.addEventListener('click', () => {game.reset(player1, player2)});
